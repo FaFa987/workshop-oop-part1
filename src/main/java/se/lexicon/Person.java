0ce55eb196f76239ef5c8f34bdccadd5ae4b3f8e
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-30 09:29:51 +0100

    feilds

---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+public class Person {
+
+    private static int sequencer;
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/se/lexicon/book.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+public class book {
+
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+
+
+}
